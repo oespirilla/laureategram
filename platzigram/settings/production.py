e0a5f4447caaeebf87c0527c87d0c53649b08f65
Oscar Espirilla <oscar.espirilla@laureate.net>
2018-10-02 00:44:31 -0500

    separate settings for dev and prod

---
+++ platzigram/settings/production.py
@@ -0,0 +1,15 @@
+import os
+
+from .base import *
+
+
+DEBUG = False
+# A different Database for production
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
